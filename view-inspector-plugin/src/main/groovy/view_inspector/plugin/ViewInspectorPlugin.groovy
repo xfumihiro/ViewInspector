986ffc6d7d6f1ce0eea30e617db33c0ac877b3fb
Fumihiro Xue <xfumihiro@gmail.com>
2015-10-29 00:28:37 +0800

    Prepare next development version

--- view-inspector-plugin/src/main/groovy/view_inspector/plugin/ViewInspectorPlugin.groovy
+++ view-inspector-plugin/src/main/groovy/view_inspector/plugin/ViewInspectorPlugin.groovy
@@ -17,7 +17,7 @@ class ViewInspectorPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.github.xfumihiro.view-inspector:view-inspector-runtime:0.1.3'
+      debugCompile 'com.github.xfumihiro.view-inspector:view-inspector-runtime:0.1.4-SNAPSHOT'
       debugCompile 'org.aspectj:aspectjrt:1.8.6'
       debugCompile('com.google.dexmaker:dexmaker:1.1') {
         transitive = true
